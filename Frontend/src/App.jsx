5408c06e58902fb40acd5a7b9fd914fb62e8535b
kaushalendra-marcus <yadavkausha4a5@gmail.com>
2025-09-27 09:41:44 +0530

    updating file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
         form.append("file", file);
 
         try {
-          const res = await fetch("http://127.0.0.1:8000/process-video", {
+          const res = await fetch("https://kaushalendra-marcus-nayan.hf.space/", {
             method: "POST",
             body: form,
           });
