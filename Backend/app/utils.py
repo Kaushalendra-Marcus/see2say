a8a008045fc315264e7a7e350479112ab0a64cd0
Kaushalendra Singh <yadavkausha4a5@gmail.com>
2025-12-20 12:27:06 +0530

    Refine docstring for text_to_speech_base64 function
    Updated docstring for clarity and consistency.

--- Backend/app/utils.py
+++ Backend/app/utils.py
@@ -4,11 +4,10 @@ from io import BytesIO
 
 def text_to_speech_base64(text: str) -> str:
     """
-    Generating the base64 audio string to convert caption statement into audio
+    Generate base64 audio string from text
     """
-    tts = gTTS(text = text,lang = "hi")
-    buf= BytesIO()
+    tts = gTTS(text=text, lang="hi")
+    buf = BytesIO()
     tts.write_to_fp(buf)
     buf.seek(0)
     return base64.b64encode(buf.read()).decode("utf-8")
-    
